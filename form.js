07c24acaa0eed417abd656837e70a1f3e3476dc9
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-03-31 16:04:27 +0200

    Add email validation

--- form.js
+++ form.js
@@ -1,11 +1,25 @@
 const usernameField = document.getElementById('form-username');
 const emailField = document.getElementById('form-email');
+const emailErrorField = document.getElementById('email-error');
 const passwordField = document.getElementById('form-password');
 const passwordReField = document.getElementById('form-password-re');
 
 const registrationForm = document.getElementById('form-registration');
-
 registrationForm.addEventListener("submit", handleSubmit)
+
+emailField.addEventListener("change", handleEmailChange);
+
+registrationForm.classList.add("ez-egy-class")
+registrationForm.classList.remove("ez-egy-class")
+
+function handleEmailChange(e) {
+    e.preventDefault()
+    if(emailField.value.indexOf('@') === -1){
+        emailErrorField.classList.add('error');
+    } else {
+        emailErrorField.classList.remove('error');
+    }
+}
 
 function handleSubmit(e) {
     e.preventDefault();
@@ -13,7 +27,7 @@ function handleSubmit(e) {
         emailField.value !== "" &&
         passwordField.value !== "" &&
         passwordReField.value !== "") {
-            alert("Siker");
+        alert("Siker");
     } else {
         alert("Nem siker");
     }
