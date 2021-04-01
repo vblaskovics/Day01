c60bbd511902104da2efe2c9bb60712b8279c38f
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-04-01 11:41:53 +0200

    Add formGroups

--- form.js
+++ form.js
@@ -2,11 +2,23 @@
 
 (function () {
     const form = document.getElementById('form-registration');
-    form.addEventListener("submit", handleSubmit)
+    form.addEventListener("submit", handleSubmit);
     
+    const formGroups = {};
+    const formData = {};
+    const isError = {};
+
+    // {
+    //     username: ...,
+    //     email: ...,
+    // }
+    const formFields = document.querySelectorAll('.form-registration input');
+    formFields.forEach((field) => {
+        field.name
+    })
+
     function handleSubmit(e) {
         e.preventDefault();
         alert('Siker');
     }
 })()
-
