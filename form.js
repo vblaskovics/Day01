a2cb3eacef68f4f49e7e0205c7210c51650c8ba2
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-04-01 10:56:09 +0200

    A new begining

--- form.js
+++ form.js
@@ -1,48 +1,12 @@
-const usernameField = document.getElementById('form-username');
-const emailField = document.getElementById('form-email');
-const emailErrorField = document.getElementById('email-error');
-const passwordField = document.getElementById('form-password');
-const passwordReField = document.getElementById('form-password-re');
+"use strict";
 
-const registrationForm = document.getElementById('form-registration');
-registrationForm.addEventListener("submit", handleSubmit)
-
-emailField.addEventListener("blur", handleEmailChange);
-
-function handleEmailChange(e) {
-    e.preventDefault()
-    if(emailField.value === "") {
-        emailErrorField.classList.add('error');
-        emailField.classList.add('error');
-        emailErrorField.innerHTML = "Kötelező megadni!";
-        return;
-    } else {
-        emailErrorField.classList.remove('error');
-        emailField.classList.remove('error');
-    }
-
-    if(emailField.value.indexOf('@') === -1){
-        emailErrorField.classList.add('error');
-        emailErrorField.innerHTML = "Hibás email cím!"
-        emailField.classList.add('error');
-    } else {
-        emailErrorField.classList.remove('error');
-        emailField.classList.remove('error');
-    }
+(function () {
+    const form = document.getElementById('form-registration');
+    form.addEventListener("submit", handleSubmit)
     
-
-
-}
-
-function handleSubmit(e) {
-    e.preventDefault();
-    if (usernameField.value !== "" &&
-        emailField.value !== "" &&
-        passwordField.value !== "" &&
-        passwordReField.value !== "") {
-        alert("Siker");
-    } else {
-        alert("Nem siker");
+    function handleSubmit(e) {
+        e.preventDefault();
+        alert('Siker');
     }
+})()
 
-}
